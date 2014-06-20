7820751906b2e434d57ddc3c026fc902d9f81f4b
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2014-06-20 18:53:04 +0200

    Edit hopest.{c,h} files

--- src/hopest.c
+++ src/hopest.c
@@ -1,86 +1,88 @@
 /*
-  This file is part of con4m.
-  con4m is a C library for the parallel adaptive discretization and solution
-  of partial differential equations (PDEs).  It relies on p4est and libsc.
+  This file is part of hopest.
+  hopest is a Fortran/C library and application for high-order mesh
+  preprocessing and interfacing to the p4est apaptive mesh library.
 
-  Copyright (C) 2013, 2014 Carsten Burstedde and others.
+  Copyright (C) 2014 by the developers.
 
-  con4m is free software; you can redistribute it and/or modify
+  hopest is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
-  con4m is distributed in the hope that it will be useful,
+  hopest is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with con4m; if not, write to the Free Software Foundation, Inc.,
+  along with hopest; if not, write to the Free Software Foundation, Inc.,
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-#include <con4m.h>
+#include <hopest.h>
 
-static int          con4m_package_id = -1;
+static int          hopest_package_id = -1;
 
 int
-con4m_get_package_id (void)
+hopest_get_package_id (void)
 {
-  return con4m_package_id;
+  return hopest_package_id;
 }
 
 static void
-con4m_logv (int category, int priority, const char *fmt, va_list ap)
+hopest_logv (int category, int priority, const char *fmt, va_list ap)
 {
-  sc_logv ("unknown", -1, con4m_package_id, category, priority, fmt, ap);
+  sc_logv ("unknown", -1, hopest_package_id, category, priority, fmt, ap);
 }
 
 void
-con4m_logf (int category, int priority, const char *fmt, ...)
-{
-  va_list             ap;
-
-  va_start (ap, fmt);
-  sc_logv ("unknown", -1, con4m_package_id, category, priority, fmt, ap);
-  va_end (ap);
-}
-
-void
-con4m_global_essentialf (const char *fmt, ...)
+hopest_logf (int category, int priority, const char *fmt, ...)
 {
   va_list             ap;
 
   va_start (ap, fmt);
-  con4m_logv (SC_LC_GLOBAL, SC_LP_ESSENTIAL, fmt, ap);
+  sc_logv ("unknown", -1, hopest_package_id, category, priority, fmt, ap);
   va_end (ap);
 }
 
 void
-con4m_global_productionf (const char *fmt, ...)
+hopest_global_essentialf (const char *fmt, ...)
 {
   va_list             ap;
 
   va_start (ap, fmt);
-  con4m_logv (SC_LC_GLOBAL, SC_LP_PRODUCTION, fmt, ap);
+  hopest_logv (SC_LC_GLOBAL, SC_LP_ESSENTIAL, fmt, ap);
   va_end (ap);
 }
 
 void
-con4m_init (sc_log_handler_t log_handler, int log_threshold)
+hopest_global_productionf (const char *fmt, ...)
+{
+  va_list             ap;
+
+  va_start (ap, fmt);
+  hopest_logv (SC_LC_GLOBAL, SC_LP_PRODUCTION, fmt, ap);
+  va_end (ap);
+}
+
+void
+hopest_init (sc_log_handler_t log_handler, int log_threshold)
 {
   int                 w;
 
-  con4m_package_id = sc_package_register (log_handler, log_threshold,
-                                          "con4m",
-                                          "Adaptive discretizations");
+  hopest_package_id = sc_package_register (log_handler, log_threshold,
+                                          "hopest",
+                                          "High-order mesh preprocessor");
 
   w = 24;
-  con4m_global_essentialf ("This is %s\n", CON4M_PACKAGE_STRING);
-  con4m_global_productionf ("%-*s %s\n", w, "CPP", CON4M_CPP);
-  con4m_global_productionf ("%-*s %s\n", w, "CPPFLAGS", CON4M_CPPFLAGS);
-  con4m_global_productionf ("%-*s %s\n", w, "CC", CON4M_CC);
-  con4m_global_productionf ("%-*s %s\n", w, "CFLAGS", CON4M_CFLAGS);
-  con4m_global_productionf ("%-*s %s\n", w, "LDFLAGS", CON4M_LDFLAGS);
-  con4m_global_productionf ("%-*s %s\n", w, "LIBS", CON4M_LIBS);
+  hopest_global_essentialf ("This is %s\n", HOPEST_PACKAGE_STRING);
+  hopest_global_productionf ("%-*s %s\n", w, "CPP", HOPEST_CPP);
+  hopest_global_productionf ("%-*s %s\n", w, "CPPFLAGS", HOPEST_CPPFLAGS);
+  hopest_global_productionf ("%-*s %s\n", w, "F77", HOPEST_F77);
+  hopest_global_productionf ("%-*s %s\n", w, "FFLAGS", HOPEST_FFLAGS);
+  hopest_global_productionf ("%-*s %s\n", w, "CC", HOPEST_CC);
+  hopest_global_productionf ("%-*s %s\n", w, "CFLAGS", HOPEST_CFLAGS);
+  hopest_global_productionf ("%-*s %s\n", w, "LDFLAGS", HOPEST_LDFLAGS);
+  hopest_global_productionf ("%-*s %s\n", w, "LIBS", HOPEST_LIBS);
 }
--- src/hopest.h
+++ src/hopest.h
@@ -1,80 +1,80 @@
 /*
-  This file is part of con4m.
-  con4m is a C library for the parallel adaptive discretization and solution
-  of partial differential equations (PDEs).  It relies on p4est and libsc.
+  This file is part of hopest.
+  hopest is a Fortran/C library and application for high-order mesh
+  preprocessing and interfacing to the p4est apaptive mesh library.
 
-  Copyright (C) 2013, 2014 Carsten Burstedde and others.
+  Copyright (C) 2014 by the developers.
 
-  con4m is free software; you can redistribute it and/or modify
+  hopest is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
-  con4m is distributed in the hope that it will be useful,
+  hopest is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with con4m; if not, write to the Free Software Foundation, Inc.,
+  along with hopest; if not, write to the Free Software Foundation, Inc.,
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 /*
- * This is a file that every program using con4m should include,
+ * This is a file that every program using hopest should include,
  * directly or indirectly.  It pulls in subpackage definitions.
  */
 
-#ifndef CON4M_H
-#define CON4M_H
+#ifndef HOPEST_H
+#define HOPEST_H
 
 /* include config headers */
-#include <con4m_config.h>
+#include <hopest_config.h>
 #include <sc_config.h>
 #if \
-  (defined (CON4M_MPI) && !defined (SC_MPI)) || \
-  (!defined (CON4M_MPI) && defined (SC_MPI))
-#error "MPI configured differently in con4m and libsc"
+  (defined (HOPEST_MPI) && !defined (SC_MPI)) || \
+  (!defined (HOPEST_MPI) && defined (SC_MPI))
+#error "MPI configured differently in hopest and libsc"
 #endif
 #if \
-  (defined (CON4M_MPIIO) && !defined (SC_MPIIO)) || \
-  (!defined (CON4M_MPIIO) && defined (SC_MPIIO))
-#error "MPI I/O configured differently in con4m and libsc"
+  (defined (HOPEST_MPIIO) && !defined (SC_MPIIO)) || \
+  (!defined (HOPEST_MPIIO) && defined (SC_MPIIO))
+#error "MPI I/O configured differently in hopest and libsc"
 #endif
 
 /* indirectly also include p4est_config.h, sc.h, sc_containers.h */
 #include <p4est_base.h>
-#define _con4m_const _sc_const
-#define _con4m_restrict _sc_restrict
+#define _hopest_const _sc_const
+#define _hopest_restrict _sc_restrict
 
 /* macros for memory allocation, will abort if out of memory */
-#define CON4M_ALLOC(t,n)          (t *) sc_malloc (con4m_get_package_id (), \
+#define HOPEST_ALLOC(t,n)          (t *) sc_malloc (hopest_get_package_id (), \
                                                    (n) * sizeof(t))
-#define CON4M_ALLOC_ZERO(t,n)     (t *) sc_calloc (con4m_get_package_id (), \
+#define HOPEST_ALLOC_ZERO(t,n)     (t *) sc_calloc (hopest_get_package_id (), \
                                                    (size_t) (n), sizeof(t))
-#define CON4M_REALLOC(p,t,n)      (t *) sc_realloc (con4m_get_package_id (), \
+#define HOPEST_REALLOC(p,t,n)      (t *) sc_realloc (hopest_get_package_id (), \
                                                     (p), (n) * sizeof(t))
-#define CON4M_STRDUP(s)                 sc_strdup (con4m_get_package_id (), (s))
-#define CON4M_FREE(p)                   sc_free (con4m_get_package_id (), (p))
+#define HOPEST_STRDUP(s)                 sc_strdup (hopest_get_package_id (), (s))
+#define HOPEST_FREE(p)                   sc_free (hopest_get_package_id (), (p))
 
 /* some error checking */
-#ifdef CON4M_DEBUG
-#define CON4M_ASSERT(c) SC_CHECK_ABORT ((c), "Assertion '" #c "'")
+#ifdef HOPEST_DEBUG
+#define HOPEST_ASSERT(c) SC_CHECK_ABORT ((c), "Assertion '" #c "'")
 #else
-#define CON4M_ASSERT(c) SC_NOOP ()
+#define HOPEST_ASSERT(c) SC_NOOP ()
 #endif
 
 /* functions for mananging the package identity within libsc */
-int                 con4m_get_package_id (void);
+int                 hopest_get_package_id (void);
 
 /* functions for printing log messages */
-void                con4m_logf (int category, int priority,
+void                hopest_logf (int category, int priority,
                                 const char *fmt, ...);
-void                con4m_global_essentialf (const char *fmt, ...);
-void                con4m_global_productionf (const char *fmt, ...);
+void                hopest_global_essentialf (const char *fmt, ...);
+void                hopest_global_productionf (const char *fmt, ...);
 
-/* register con4m with libsc and print version and variable information */
-void                con4m_init (sc_log_handler_t log_handler,
+/* register hopest with libsc and print version and variable information */
+void                hopest_init (sc_log_handler_t log_handler,
                                 int log_threshold);
 
-#endif /* !CON4M_H */
+#endif /* !HOPEST_H */
