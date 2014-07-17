2ab0840e97d80ecd3b73bba350bb34a7d5f9883c
Florian Hindenlang <iaghinde@lax.iag.uni-stuttgart.de>
2014-07-17 23:35:44 +0200

    neuer ordner examples/hopest mit der hopest.f90, wird im root makefile angegeben.

---
+++ example/hopest/hopest.f90
@@ -0,0 +1,46 @@
+#include "../../src/hopest_f.h"
+
+PROGRAM Hopest
+!===================================================================================================================================
+! Control program of the Flexi code. Initialization of the computation
+!===================================================================================================================================
+! MODULES
+USE MOD_Globals
+USE MOD_ReadInTools,  ONLY:IgnoredStrings
+USE MOD_Mesh,         ONLY:InitMesh,FinalizeMesh
+USE MOD_IO_HDF5,      ONLY:InitIO
+USE MOD_MPI,          ONLY:InitMPI
+
+! IMPLICIT VARIABLE HANDLING
+IMPLICIT NONE
+!-----------------------------------------------------------------------------------------------------------------------------------
+! LOCAL VARIABLES 
+REAL :: Time
+!===================================================================================================================================
+CALL InitMPI()
+CALL InitIO()
+
+! hier fehlt der HEADER
+
+! Measure init duration
+StartTime=RUNTIME()
+
+! Initialization
+CALL InitMesh()
+CALL IgnoredStrings()
+
+!Finalize
+CALL FinalizeMesh()
+! Measure simulation duration
+Time=RUNTIME()
+#ifdef MPI
+CALL MPI_FINALIZE(iError)
+IF(iError .NE. 0) &
+  CALL abort(__STAMP__, &
+  'MPI finalize error',iError,999.)
+#endif
+SWRITE(UNIT_stdOut,'(132("="))')
+SWRITE(UNIT_stdOut,'(A,F8.2,A)') ' HOPEST FINISHED! [',Time-StartTime,' sec ]'
+SWRITE(UNIT_stdOut,'(132("="))')
+
+END PROGRAM Hopest
