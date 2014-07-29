fa5f80f8d6c7664c6653ac19104c3d8abed74b01
Florian Hindenlang <iaghinde@lax.iag.uni-stuttgart.de>
2014-07-29 19:13:06 +0200

    formatting of the fortran binding routine, with input output variables.

--- src/cwrapper/p4est_binding.f90
+++ src/cwrapper/p4est_binding.f90
@@ -12,29 +12,59 @@ IMPLICIT NONE
 INTERFACE 
 
   SUBROUTINE p4est_connectivity_treevertex(num_vertices,num_trees,vertices,tree_to_vertex,p4est) BIND(C)
-    USE, INTRINSIC :: ISO_C_BINDING  
-    IMPLICIT NONE
-    INTEGER( KIND = C_INT),VALUE     :: num_vertices 
-    INTEGER( KIND = C_INT),VALUE     :: num_trees 
-    REAL( KIND = C_DOUBLE )          :: Vertices(3*num_vertices)
-    INTEGER( KIND = C_INT)           :: tree_to_vertex(8*num_trees) 
-    TYPE(C_PTR)                      :: p4est
+  !=================================================================================================================================
+  ! builds up p4est connectivit, using only element connectivity and vertex positions
+  !=================================================================================================================================
+  ! MODULES
+  USE, INTRINSIC :: ISO_C_BINDING  
+  ! IMPLICIT VARIABLE HANDLING
+  IMPLICIT NONE
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! INPUT VARIABLES
+  INTEGER( KIND = C_INT),VALUE     :: num_vertices 
+  INTEGER( KIND = C_INT),VALUE     :: num_trees 
+  REAL( KIND = C_DOUBLE )          :: Vertices(3*num_vertices)
+  INTEGER( KIND = C_INT)           :: tree_to_vertex(8*num_trees) 
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! OUTPUT VARIABLES
+  TYPE(C_PTR)                      :: p4est
+  !=================================================================================================================================
   END SUBROUTINE p4est_connectivity_treevertex 
 
   SUBROUTINE p4est_refine_mesh(p4est,refine_level,refine_elem,mesh) BIND(C)
-    USE, INTRINSIC :: ISO_C_BINDING  
-    IMPLICIT NONE
-    TYPE(C_PTR),VALUE                :: p4est
-    INTEGER( KIND = C_INT),VALUE     :: refine_level 
-    INTEGER( KIND = C_INT),VALUE     :: refine_elem 
-    TYPE(C_PTR)                      :: mesh
+  !=================================================================================================================================
+  ! simple refine function, giving the level and if refine_elem < 0 then a conformal refinement is applied.
+  !=================================================================================================================================
+  ! MODULES
+  USE, INTRINSIC :: ISO_C_BINDING  
+  ! IMPLICIT VARIABLE HANDLING
+  IMPLICIT NONE
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! INPUT VARIABLES
+  TYPE(C_PTR),VALUE                :: p4est
+  INTEGER( KIND = C_INT),VALUE     :: refine_level 
+  INTEGER( KIND = C_INT),VALUE     :: refine_elem 
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! OUTPUT VARIABLES
+  TYPE(C_PTR)                      :: mesh
+  !=================================================================================================================================
   END SUBROUTINE p4est_refine_mesh 
 
   SUBROUTINE p4est_save_all(filename, p4est) BIND(C)
-    USE, INTRINSIC :: ISO_C_BINDING  
-    IMPLICIT NONE
-    CHARACTER(KIND=C_CHAR),DIMENSION(*) :: filename
-    TYPE(C_PTR),VALUE                   :: p4est
+  !=================================================================================================================================
+  ! save the p4est data  to a p4est state file 
+  !=================================================================================================================================
+  ! MODULES
+  USE, INTRINSIC :: ISO_C_BINDING  
+  ! IMPLICIT VARIABLE HANDLING
+  IMPLICIT NONE
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! INPUT VARIABLES
+  CHARACTER(KIND=C_CHAR),DIMENSION(*) :: filename
+  TYPE(C_PTR),VALUE                   :: p4est
+  !---------------------------------------------------------------------------------------------------------------------------------
+  ! OUTPUT VARIABLES
+  !=================================================================================================================================
   END SUBROUTINE p4est_save_all
 
 END INTERFACE
